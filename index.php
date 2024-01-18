dc84284975b108145354e082f73eaea62a199b1f
Lillian nelson <nelson.lillian@student.greenriver.edugit config --global user.email lanelson197@gmail.comgit config --global user.email nelson.lillian@student.greenriver.edu>
2024-01-18 13:02:59 -0500

    added to view

--- index.php
+++ index.php
@@ -16,9 +16,10 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 $f3->route('GET /', function() {
-    echo "<h1>Pet Home</h1>";
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
-$f3->run();f
+$f3->run();
 
 ?>
